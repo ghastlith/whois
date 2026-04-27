0eb245be71291cf6a19e22325f9d5b547b8a41bf
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-04-27 18:54:04 -0300

    refactor: change kts file tabs to match industry standards

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,41 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.5.4"
-    id("io.spring.dependency-management") version "1.1.7"
+  java
+  id("org.springframework.boot") version "3.5.4"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "ghastlith"
 version = "1.0.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 dependencies {
-    // spring
-    implementation("org.springframework.boot:spring-boot-starter-web:3.5.4")
+  // spring
+  implementation("org.springframework.boot:spring-boot-starter-web:3.5.4")
 
-    // tests
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.4")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+  // tests
+  testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.4")
+  testImplementation("org.assertj:assertj-core:3.24.2")
 
-    // lombok
-    compileOnly("org.projectlombok:lombok:1.18.38")
-    annotationProcessor("org.projectlombok:lombok:1.18.38")
-    testCompileOnly("org.projectlombok:lombok:1.18.38")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+  // lombok
+  compileOnly("org.projectlombok:lombok:1.18.38")
+  annotationProcessor("org.projectlombok:lombok:1.18.38")
+  testCompileOnly("org.projectlombok:lombok:1.18.38")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
+  useJUnitPlatform()
 
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
 }
