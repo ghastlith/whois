f92a8bdaecfc93d20349f3ed754b675bc67c0d98
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-05-09 20:12:00 -0300

    chore: add semicolons to kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,40 @@
 plugins {
-  java
-  id("org.springframework.boot") version "3.5.4"
-  id("io.spring.dependency-management") version "1.1.7"
+  java;
+  id("org.springframework.boot") version "3.5.14";
+  id("io.spring.dependency-management") version "1.1.7";
 }
 
-group = "ghastlith"
-version = "1.0.0"
+group = "ghastlith";
+version = "1.0.0";
 
 repositories {
-  mavenCentral()
+  mavenCentral();
 }
 
 java {
   toolchain {
-    languageVersion = JavaLanguageVersion.of(21)
+    languageVersion = JavaLanguageVersion.of(21);
   }
 }
 
 dependencies {
   // spring
-  implementation("org.springframework.boot:spring-boot-starter-web:3.5.4")
+  implementation("org.springframework.boot:spring-boot-starter-web:3.5.14");
 
   // tests
-  testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.4")
+  testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.14");
 
   // lombok
-  compileOnly("org.projectlombok:lombok:1.18.38")
-  annotationProcessor("org.projectlombok:lombok:1.18.38")
-  testCompileOnly("org.projectlombok:lombok:1.18.38")
-  testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+  compileOnly("org.projectlombok:lombok:1.18.38");
+  annotationProcessor("org.projectlombok:lombok:1.18.38");
+  testCompileOnly("org.projectlombok:lombok:1.18.38");
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.38");
 }
 
 tasks.withType<Test>().configureEach {
-  useJUnitPlatform()
+  useJUnitPlatform();
 
   testLogging {
-    events("passed", "skipped", "failed")
+    events("passed", "skipped", "failed");
   }
 }
